dcdc0fdf201ecf7f8e7e80b13c50e7742714c61c
xijunsun <xijun.sun@polyu.edu.hk>
2025-06-30 15:50:26 +0800

    minor delete

--- visualization_m.py
+++ visualization_m.py
@@ -10,18 +10,14 @@ point_biserial_analysis(df, 'Modic_change_L3', 'SIWeightedCentroid_XCOR',SAVE_FO
 point_biserial_analysis(df, 'Modic_change_L3', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L3', 'Mean',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L3', 'Coeff of Variation',SAVE_FOLDER)
-point_biserial_analysis(df, 'Modic_change_L3', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
 
 point_biserial_analysis(df, 'Modic_change_L4', 'SIWeightedCentroid_XCOR',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L4', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L4', 'Mean',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L4', 'Coeff of Variation',SAVE_FOLDER)
-point_biserial_analysis(df, 'Modic_change_L4', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
 
 point_biserial_analysis(df, 'Modic_change_L5', 'SIWeightedCentroid_XCOR',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L5', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L5', 'Mean',SAVE_FOLDER)
 point_biserial_analysis(df, 'Modic_change_L5', 'Coeff of Variation',SAVE_FOLDER)
-point_biserial_analysis(df, 'Modic_change_L5', 'SIWeightedCentroid_YCOR',SAVE_FOLDER)
-
 
